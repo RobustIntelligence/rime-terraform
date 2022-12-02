d8eefae1a7f9d5755a762cec7b84b6cd0c065ce4
Robust Intelligence <ri-hlin@users.noreply.github.com>
2022-12-02 06:39:50 +0000

    Automated RIME-Terraform module publish for 0.20.3 on 20221202_063950

--- rime_eks_cluster/variables.tf
+++ rime_eks_cluster/variables.tf
@@ -74,7 +74,7 @@ variable "model_testing_worker_group_max_size" {
 }
 
 variable "model_testing_worker_groups_overrides" {
-  description = "A dict of overrides for the model testing worker group launch templates. See https://github.com/terraform-aws-modules/terraform-aws-eks/blob/v17.24.0/locals.tf#L36 for valid values."
+  description = "A dictionary that specifies overrides for the model testing worker group launch templates. See https://github.com/terraform-aws-modules/terraform-aws-eks/blob/v17.24.0/locals.tf#L36 for valid values."
   type        = any
   default     = {}
 }
@@ -115,7 +115,7 @@ variable "server_worker_group_max_size" {
 }
 
 variable "server_worker_groups_overrides" {
-  description = "A dict of overrides for the server worker group launch templates. See https://github.com/terraform-aws-modules/terraform-aws-eks/blob/v17.24.0/locals.tf#L36 for valid values."
+  description = "A dictionary that specifies overrides for the server worker group launch templates. See https://github.com/terraform-aws-modules/terraform-aws-eks/blob/v17.24.0/locals.tf#L36 for valid values."
   type        = any
   default     = {}
 }
