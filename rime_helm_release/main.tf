5166f3080e52c04bb723502a452aec45b42061a6
Robust Intelligence <ri-hlin@users.noreply.github.com>
2023-02-16 01:39:12 +0000

    Automated RIME-Terraform module publish for 0.20.17 on 20230216_013912

--- rime_helm_release/main.tf
+++ rime_helm_release/main.tf
@@ -1,15 +1,6 @@
 locals {
   is_namespace_default = (var.namespace == "default")
-  mongo_storage_class  = local.is_namespace_default ? "mongo-storage" : "mongo-storage-${var.namespace}"
   tags                 = join(",", [for key, value in var.tags : "${key}=${value}"])
-}
-
-resource "random_password" "jwt_secret" {
-  length  = 64
-  lower   = true
-  number  = true
-  special = true
-  upper   = true
 }
 
 resource "kubernetes_namespace" "namespace" {
@@ -47,6 +38,7 @@ module "blob_store" {
   namespace            = var.namespace
   oidc_provider_url    = var.oidc_provider_url
   resource_name_suffix = var.resource_name_suffix
+  force_destroy        = var.force_destroy
   tags                 = var.tags
 }
 
@@ -56,11 +48,13 @@ module "image_registry" {
 
   count = var.enable_image_registry ? 1 : 0
 
-  namespace            = var.namespace
-  oidc_provider_url    = var.oidc_provider_url
-  repository_prefix    = var.image_registry_config.repo_base_name
-  resource_name_suffix = var.resource_name_suffix
-  tags                 = var.tags
+  cloud_platform_config = var.cloud_platform_config
+
+  namespace             = var.namespace
+  oidc_provider_url     = var.oidc_provider_url
+  image_registry_config = var.image_registry_config
+  resource_name_suffix  = var.resource_name_suffix
+  tags                  = var.tags
 }
 
 // Create secret "rimecreds" in each namespace if we created the namespace
@@ -91,27 +85,18 @@ resource "local_file" "helm_values" {
 
     blob_store_config = {
       enable         = var.enable_blob_store
-      s3_bucket_name = var.enable_blob_store ? module.blob_store[0].blob_store_bucket_arn : ""
+      s3_bucket_name = var.enable_blob_store ? module.blob_store[0].blob_store_bucket_name : ""
       role_arn       = var.enable_blob_store ? module.blob_store[0].blob_store_role_arn : ""
     }
 
-    docker_secret_name  = var.docker_secret_name
-    docker_registry     = var.docker_registry
-    domain              = var.domain == "" ? "placeholder" : var.domain
-    enable_api_key_auth = var.enable_api_key_auth
+    docker_secret_name    = var.docker_secret_name
+    docker_registry       = var.docker_registry
+    domain                = var.domain == "" ? "placeholder" : var.domain
+    enable_api_key_auth   = var.enable_api_key_auth
+    enable_external_agent = var.enable_external_agent
 
-    image_registry_config = {
-      registry_type                = var.image_registry_config.enable ? "ecr" : null
-      allow_external_custom_images = true
-      ecr_config = var.image_registry_config.enable ? {
-        registry_id       = module.image_registry[0].ecr_registry_id
-        repository_prefix = module.image_registry[0].unique_repository_prefix
-      } : null
-      image_builder_role_arn = module.image_registry[0].ecr_image_builder_role_arn
-      repo_manager_role_arn  = module.image_registry[0].ecr_repo_manager_role_arn
-    }
+    image_registry_config = module.image_registry[0].image_registry_config
 
-    jwt_secret                   = random_password.jwt_secret.result
     lb_tags                      = length(local.tags) > 0 ? "service.beta.kubernetes.io/aws-load-balancer-additional-resource-tags: \"${local.tags}\"" : ""
     lb_type                      = var.internal_lbs ? "internal" : "internet-facing"
     mongo_db_size                = var.mongo_db_size
@@ -125,6 +110,7 @@ resource "local_file" "helm_values" {
     ip_allowlist                 = var.ip_allowlist
     separate_model_testing_group = var.separate_model_testing_group
     release_name                 = var.release_name
+    datadog_tag_pod_annotation   = var.datadog_tag_pod_annotation
   })
   filename = format("%s/values_%s.yaml", length(var.helm_values_output_dir) == 0 ? "${path.cwd}" : var.helm_values_output_dir, var.namespace)
 }
@@ -148,7 +134,8 @@ resource "helm_release" "rime" {
   wait              = false
 
   values = [
-    local_file.helm_values.content
+    local_file.helm_values.content,
+    length(var.override_values_file_path) > 0 ? file(var.override_values_file_path) : "",
   ]
   depends_on = [kubernetes_namespace.namespace]
 }
