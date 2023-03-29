47311173d8b68af8179bf3deff6b04815272c136
Robust Intelligence <ri-hlin@users.noreply.github.com>
2023-03-29 21:07:10 +0000

    Automated RIME-Terraform module publish for 2.0.0-beta.18 on 20230329_210710

--- rime_agent/versions.tf
+++ rime_agent/versions.tf
@@ -7,24 +7,9 @@ terraform {
       version = ">= 3.20.0, < 4.0.0"
     }
 
-    kubernetes =  {
+    kubernetes = {
       source  = "hashicorp/kubernetes"
       version = ">= 2.0.1, < 3.0.0"
-    }
-
-    local = {
-      source  = "hashicorp/local"
-      version = "2.0.0"
-    }
-
-    null = {
-      source  = "hashicorp/null"
-      version = "3.0.0"
-    }
-
-    random = {
-      source  = "hashicorp/random"
-      version = "3.0.0"
     }
   }
 }
